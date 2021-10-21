5c1b4503d335cacb3d7ced86749f7691344b1074
Matt Scilipoti <matt@scilipoti.name>
2021-10-20 21:46:59 -0400

    Run specs for the installed gem
    - extract shared specs for library and installed gem
    - gem requires html validator classes
    - rubocop ignores shared_specs

--- spec/vcr_helper.rb
+++ spec/vcr_helper.rb
@@ -2,7 +2,7 @@
 
 require "vcr"
 
-VCR.config do |c|
+VCR.configure do |c|
   c.cassette_library_dir = "spec/cassettes"
   c.configure_rspec_metadata! # https://relishapp.com/vcr/vcr/v/6-0-0/docs/test-frameworks/usage-with-rspec-metadata
   c.default_cassette_options = { record: :new_episodes }

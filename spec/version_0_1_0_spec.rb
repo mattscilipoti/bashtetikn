995f0729903305349934a2cbe94a920592e0f726
Matt Scilipoti <matt@scilipoti.name>
2021-10-20 22:06:42 -0400

    Spec the gem: installs the gem when necessary

--- spec/version_0_1_0_spec.rb
+++ spec/version_0_1_0_spec.rb
@@ -4,14 +4,19 @@ require_relative "shared/shared_html_validator_specs"
 
 EXPECTED_VERSION = "0.1.0"
 
-# NOTE: you must install the gem locally before running these, via:
-# - `rake build`
-# - `gem install pkg/bashtetikn-#{EXPECTED_VERSION}.gem`
-
 RSpec.describe "The installed gem (#{EXPECTED_VERSION})" do
-  # test the built, installed gem
-  gem "bashtetikn", EXPECTED_VERSION
-  require "bashtetikn"
+  begin
+    # test the built, installed gem
+    gem "bashtetikn", EXPECTED_VERSION
+    require "bashtetikn"
+  rescue LoadError
+    # NOTE: you must install the gem locally before running these, via:
+    unless system("rake build") && system("gem install pkg/bashtetikn-#{EXPECTED_VERSION}.gem")
+      puts "ERROR: building and installing gem. Exiting specs."
+      exit 1
+    end
+    retry
+  end
 
   it "version should be #{EXPECTED_VERSION}" do
     expect(Bashtetikn::VERSION).to eql(EXPECTED_VERSION)
